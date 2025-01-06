ece0aba716c43ea01e259786908a15ba43459a36
jegp <jens@jepedersen.dk>
2025-01-06 19:57:21 +0000

    Avoided redundant libcaer installations

--- .github/workflows/install_libcaer.sh
+++ .github/workflows/install_libcaer.sh
@@ -11,14 +11,18 @@ elif [ "$(uname)" == "MINGW"* ] || [ "$(uname)" == "MSYS"* ]; then
     vcpkg install libusb:x64-windows
 fi
 
-# Clone and build libcaer
-git clone https://gitlab.com/inivation/dv/libcaer.git
-cd libcaer
-
-if [ "$(uname)" == "Darwin" ]; then
-    cmake -S . -B build -DENABLE_OPENCV=0 -DENABLE_SERIALDEV=0 -DCMAKE_OSX_ARCHITECTURES=x86_64
+# Check if libcaer directory already exists
+if [ ! -d "libcaer" ]; then
+    # Clone and build libcaer
+    git clone https://gitlab.com/inivation/dv/libcaer.git
+    cd libcaer
+    if [ "$(uname)" == "Darwin" ]; then
+        cmake -S . -B build -DENABLE_OPENCV=0 -DENABLE_SERIALDEV=0 -DCMAKE_OSX_ARCHITECTURES=x86_64
+    else
+        cmake -S . -B build -DENABLE_OPENCV=0 -DENABLE_SERIALDEV=0
+    fi
 else
-    cmake -S . -B build -DENABLE_OPENCV=0 -DENABLE_SERIALDEV=0
+    cd libcaer
 fi
 
 cmake --build build --config Release
